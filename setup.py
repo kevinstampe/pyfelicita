31f070bb8d3b032db19492192373a50ec5f0a08c
Kevin Stampe <kst@vertica.dk>
2024-11-16 18:17:19 +0100

    bump to 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyfelicita",
-    version="0.1.2",
+    version="0.1.3",
     description="A Bluetooth scale integration for coffee machines",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
